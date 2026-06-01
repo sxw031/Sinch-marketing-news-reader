364a1ceaf04c6fb1956a1c509d73a5ab681a0405
Jamison Wong <12452881+sxw031@users.noreply.github.com>
2026-06-01 09:07:21 +0800

    Update sources.js

--- backend/config/sources.js
+++ backend/config/sources.js
@@ -1,23 +1,234 @@
 const COMPANIES = [
-  {id: 'hsbc', name: 'HSBC', category: 'Banking & Finance', sources: [{type: 'newsapi', query: 'HSBC bank news'}, {type: 'rss', url: 'https://www.hsbc.com/rss/news.xml'}]},
-  {id: 'grab', name: 'Grab', category: 'Super App', sources: [{type: 'newsapi', query: 'Grab Southeast Asia app news'}, {type: 'rss', url: 'https://grab.com/feed/'}]},
-  {id: 'vge', name: 'VGE', category: 'Energy', sources: [{type: 'newsapi', query: 'VGE energy company news'}]},
-  {id: 'cathay', name: 'Cathay Pacific', category: 'Airlines', sources: [{type: 'newsapi', query: 'Cathay Pacific airline news'}, {type: 'rss', url: 'https://www.cathaypacific.com/news/rss'}]},
-  {id: 'alibaba', name: 'Alibaba', category: 'E-commerce', sources: [{type: 'newsapi', query: 'Alibaba e-commerce technology news'}, {type: 'rss', url: 'https://www.alibaba.com/feed'}]},
-  {id: 'charter', name: 'Charter Bank', category: 'Banking', sources: [{type: 'newsapi', query: 'Charter Bank digital banking news'}]},
-  {id: 'temu', name: 'Temu', category: 'E-commerce', sources: [{type: 'newsapi', query: 'Temu e-commerce shopping app news'}]},
-  {id: 'ctrip', name: 'Ctrip', category: 'Travel', sources: [{type: 'newsapi', query: 'Ctrip travel booking hospitality news'}, {type: 'rss', url: 'https://www.ctrip.com/feed'}]},
-  {id: 'didi', name: 'Didi', category: 'Ride-hailing', sources: [{type: 'newsapi', query: 'Didi ride-hailing mobility news'}]},
-  {id: 'pdd', name: 'PDD', category: 'E-commerce', sources: [{type: 'newsapi', query: 'PDD Holdings e-commerce news'}]},
-  {id: 'dbs', name: 'DBS', category: 'Banking', sources: [{type: 'newsapi', query: 'DBS bank Singapore financial news'}, {type: 'rss', url: 'https://www.dbs.com/rss/news.xml'}]},
-  {id: 'tencent', name: 'Tencent', category: 'Technology', sources: [{type: 'newsapi', query: 'Tencent technology gaming news'}]},
-  {id: 'bankofchina', name: 'Bank of China', category: 'Banking', sources: [{type: 'newsapi', query: 'Bank of China BOC financial services news'}]},
-  {id: 'bytedance', name: 'ByteDance', category: 'Technology', sources: [{type: 'newsapi', query: 'ByteDance technology media TikTok news'}]},
-  {id: 'gojek', name: 'Gojek', category: 'Super App', sources: [{type: 'newsapi', query: 'Gojek Southeast Asia app news'}]},
-  {id: 'citigroup', name: 'Citigroup', category: 'Banking', sources: [{type: 'newsapi', query: 'Citigroup financial services banking news'}, {type: 'rss', url: 'https://www.citigroup.com/rss/news.xml'}]},
-  {id: 'gov-singapore', name: 'Government of Singapore', category: 'Government', sources: [{type: 'newsapi', query: 'Singapore government policy news'}, {type: 'rss', url: 'https://www.gov.sg/rss'}]},
-  {id: 'binance', name: 'Binance', category: 'Cryptocurrency', sources: [{type: 'newsapi', query: 'Binance cryptocurrency blockchain news'}, {type: 'rss', url: 'https://www.binance.com/en/feed'}]},
-  {id: 'shopback', name: 'ShopBack', category: 'Fintech', sources: [{type: 'newsapi', query: 'ShopBack cashback rewards shopping news'}]},
-  {id: 'aeoncredit', name: 'Aeon Credit', category: 'Financial Services', sources: [{type: 'newsapi', query: 'Aeon Credit financial services news'}]}
+  {
+    id: 'hsbc',
+    name: 'HSBC',
+    category: 'Banking & Finance',
+    website: {
+      url: 'https://www.hsbc.com/news',
+      selectors: {
+        container: 'article.news-item, div.news-card',
+        title: 'h3, .news-title, a.news-link',
+        description: 'p.summary, .news-description',
+        link: 'a.news-link, a[href*="/news/"]',
+        date: 'time, .news-date, .published-date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.hsbc.com/rss/news.xml'}]
+  },
+  {
+    id: 'grab',
+    name: 'Grab',
+    category: 'Super App',
+    website: {
+      url: 'https://www.grab.com/press/',
+      selectors: {
+        container: 'article, div.press-release',
+        title: 'h2, h3, .title',
+        description: 'p, .description',
+        link: 'a[href*="/press/"], a',
+        date: 'time, .date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://grab.com/feed/'}]
+  },
+  {
+    id: 'vge',
+    name: 'VGE',
+    category: 'Energy',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'cathay',
+    name: 'Cathay Pacific',
+    category: 'Airlines',
+    website: {
+      url: 'https://www.cathaypacific.com/cx/en/about-us/press-release.html',
+      selectors: {
+        container: 'article, div.press-item',
+        title: 'h3, a.news-link',
+        description: 'p',
+        link: 'a',
+        date: 'time, .date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.cathaypacific.com/news/rss'}]
+  },
+  {
+    id: 'alibaba',
+    name: 'Alibaba',
+    category: 'E-commerce',
+    website: {
+      url: 'https://www.alibabagroup.com/en/news',
+      selectors: {
+        container: 'div.news-item, article',
+        title: 'h3, a.title',
+        description: 'p.summary',
+        link: 'a.title, a[href*="/news/"]',
+        date: 'span.date, time'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.alibaba.com/feed'}]
+  },
+  {
+    id: 'charter',
+    name: 'Charter Bank',
+    category: 'Banking',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'temu',
+    name: 'Temu',
+    category: 'E-commerce',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'ctrip',
+    name: 'Ctrip',
+    category: 'Travel',
+    website: {
+      url: 'https://ir.ctrip.com/news-releases',
+      selectors: {
+        container: 'div.release, article.news-item',
+        title: 'h3, a.title',
+        description: 'p',
+        link: 'a.title, a[href*="/news/"]',
+        date: 'time, .date, span.date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.ctrip.com/feed'}]
+  },
+  {
+    id: 'didi',
+    name: 'Didi',
+    category: 'Ride-hailing',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'pdd',
+    name: 'PDD',
+    category: 'E-commerce',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'dbs',
+    name: 'DBS',
+    category: 'Banking',
+    website: {
+      url: 'https://www.dbs.com/newsroom',
+      selectors: {
+        container: 'article, div.news-item',
+        title: 'h3, a.news-title',
+        description: 'p.summary, p',
+        link: 'a.news-link, a[href*="/newsroom/"]',
+        date: 'time, .date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.dbs.com/rss/news.xml'}]
+  },
+  {
+    id: 'tencent',
+    name: 'Tencent',
+    category: 'Technology',
+    website: {
+      url: 'https://www.tencent.com/en-us/news.html',
+      selectors: {
+        container: 'div.news-item, li.list-item',
+        title: 'h3, a, span.title',
+        description: 'p',
+        link: 'a',
+        date: 'span.date, time'
+      }
+    },
+    sources: []
+  },
+  {
+    id: 'bankofchina',
+    name: 'Bank of China',
+    category: 'Banking',
+    website: {
+      url: 'https://www.boc.cn/en/index.html',
+      selectors: {
+        container: 'div.news-item',
+        title: 'h3, a',
+        description: 'p',
+        link: 'a',
+        date: 'span.date'
+      }
+    },
+    sources: []
+  },
+  {
+    id: 'bytedance',
+    name: 'ByteDance',
+    category: 'Technology',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'gojek',
+    name: 'Gojek',
+    category: 'Super App',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'citigroup',
+    name: 'Citigroup',
+    category: 'Banking',
+    website: {
+      url: 'https://www.citigroup.com/citi/news',
+      selectors: {
+        container: 'article, div.news-item',
+        title: 'h2, h3, a.title',
+        description: 'p.description, p',
+        link: 'a[href*="/news/"], a.title',
+        date: 'time, span.date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.citigroup.com/rss/news.xml'}]
+  },
+  {
+    id: 'gov-singapore',
+    name: 'Government of Singapore',
+    category: 'Government',
+    website: {
+      url: 'https://www.gov.sg/news',
+      selectors: {
+        container: 'article, div.news-item',
+        title: 'h3, a.title',
+        description: 'p',
+        link: 'a',
+        date: 'time, .date'
+      }
+    },
+    sources: [{type: 'rss', url: 'https://www.gov.sg/rss'}]
+  },
+  {
+    id: 'binance',
+    name: 'Binance',
+    category: 'Cryptocurrency',
+    website: null,
+    sources: [{type: 'rss', url: 'https://www.binance.com/en/feed'}]
+  },
+  {
+    id: 'shopback',
+    name: 'ShopBack',
+    category: 'Fintech',
+    website: null,
+    sources: []
+  },
+  {
+    id: 'aeoncredit',
+    name: 'Aeon Credit',
+    category: 'Financial Services',
+    website: null,
+    sources: []
+  }
 ];
+
 module.exports = { COMPANIES };
